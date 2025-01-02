c2e0e8a657e5bcc0874b21aa1e14e30957932257
Joy Reboth <135380604+Joy-reboth-kinyua@users.noreply.github.com>
2025-01-02 00:55:50 -0800

    Initial commit

---
+++ number guesssing game.py
@@ -0,0 +1,19 @@
+import random
+
+number_to_guess = random.randint(1, 100)
+while True:
+    try:
+        guess = int(input("Guess the number between 1 and 100: "))
+
+        if guess < number_to_guess:
+            print("Too low!")
+        elif guess > number_to_guess:
+            print('too high!')
+        else:
+            print("congratulations! You guessed the number.")
+            break
+    except ValueError:
+       print("please enter a valid number")
+
+
+
